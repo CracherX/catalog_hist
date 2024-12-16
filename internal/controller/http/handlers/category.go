89335a36c9c59a24f3a45e9a266f77f9ec70112b
CracherX <Blatnoyflex@mail.ru>
2024-12-16 08:41:38 +0300

    picture fixфыв

--- internal/controller/http/handlers/category.go
+++ internal/controller/http/handlers/category.go
@@ -159,6 +159,9 @@ func (ch *CategoryHandler) AddCategory(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	dto.Response(w, http.StatusCreated, "Добавление выполнено успешно!")
 }
--- internal/controller/http/handlers/product.go
+++ internal/controller/http/handlers/product.go
@@ -268,6 +268,9 @@ func (ph *ProductHandler) AddProduct(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 	dto.Response(w, http.StatusCreated, "Добавление выполнено успешно!")
 }
