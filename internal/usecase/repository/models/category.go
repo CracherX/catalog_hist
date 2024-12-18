9b80b328f9c054a78f03ae24ec86b4f92660bf8b
CracherX <Blatnoyflex@mail.ru>
2024-12-18 03:39:18 +0300

    category hotfix

--- internal/usecase/repository/models/category.go
+++ internal/usecase/repository/models/category.go
@@ -6,7 +6,7 @@ import "time"
 type Category struct {
 	ID          int       `gorm:"primaryKey;autoIncrement" json:"id"`     // Уникальный идентификатор категории
 	Name        string    `gorm:"type:varchar(255);not null" json:"name"` // Название категории
-	Description string    `gorm:"type:text;not null" json:"description"`  // Описание категории
+	Description string    `gorm:"type:text" json:"description"`           // Описание категории
 	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`       // Время создания записи
 	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updated_at"`       // Время обновления записи
 }
