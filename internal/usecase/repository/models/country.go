0f693c4cecb3723e8e3f625bcba21d075ebae75c
CracherX <Blatnoyflex@mail.ru>
2024-12-18 03:25:41 +0300

    country code hotfix

--- internal/usecase/repository/models/country.go
+++ internal/usecase/repository/models/country.go
@@ -4,9 +4,9 @@ import "time"
 
 // Country представляет страну, связанную с товаром
 type Country struct {
-	ID   int    `gorm:"primaryKey;autoIncrement" json:"id"`          // Уникальный идентификатор страны
-	Name string `gorm:"type:varchar(255);not null" json:"name"`      // Название страны
-	Code string `gorm:"type:varchar(3);not null;unique" json:"code"` // Код страны (ISO 3166-1 Alpha-3)
+	ID   int    `gorm:"primaryKey;autoIncrement" json:"id"`     // Уникальный идентификатор страны
+	Name string `gorm:"type:varchar(255);not null" json:"name"` // Название страны
+	Code string `gorm:"type:varchar(3)" json:"code"`            // Код страны (ISO 3166-1 Alpha-3)
 
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"` // Время создания записи
 	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"` // Время обновления записи
